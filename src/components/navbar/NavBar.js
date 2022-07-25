109af5a60c9e7c9a8fc9c9a85479360772d79bc8
gaston <srgaston1506@gmail.com>
2022-07-25 07:26:05 -0300

    agregue bootstrap, pronto cambio a tailwind

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#Albumes">Música</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#Albumes">Albumes</Nav.Link>
+            <Nav.Link href="#Singles">Singles</Nav.Link>
+            <Nav.Link href="#Artistas">Artistas</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default NavBar
