060fa3f59f54e6eb4b582bb3e2f1989b66d6e7e0
gaston <srgaston1506@gmail.com>
2022-08-03 15:44:38 -0300

    agregue el contador con un botón

--- src/components/Items/ItemListContainer.js
+++ src/components/Items/ItemListContainer.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Title from '../Title/Title'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItemListContainer = ({texto}) => {
+  const onAdd = (quantity) =>{
+    console.log(`Compraste ${quantity} unidades`)
+  }
+
   return (
-    <div><Title greeting={texto}/></div>
+    <div>
+      <Title greeting={texto}/>
+      <ItemCount initial={0} stock={5} onAdd={onAdd}/>
+    </div>
   )
 }
 
