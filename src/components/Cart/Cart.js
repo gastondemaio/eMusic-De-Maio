c43aa338bf0636d4bcf5e12850d5946553c5016a
gaston <srgaston1506@gmail.com>
2022-08-29 21:29:46 -0300

    agregue orden de compra

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext'
@@ -6,6 +7,27 @@ import ItemCart from '../ItemCart/ItemCart';
 const Cart = () => {
   const { cart, totalPrice} = useCartContext();
   const { clearCart } = useCartContext()
+
+  const order = {
+    buyer: {
+      name: 'Gaston',
+      email: 'gastondemaio@gmail.com',
+      phone: '3624112233',
+      addres: 'Calle Siempre Alegre 54'
+    },
+    item: cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+    total: totalPrice(),
+  }
+
+  const handleClick = () => {
+    const db = getFirestore();
+    const ordersCollection = collection(db, 'orders');
+    addDoc(ordersCollection, order)
+    .then(({id}) => console.log(id))
+
+  }
+
+
   if(cart.length === 0){
     return(
       <>
@@ -24,6 +46,7 @@ const Cart = () => {
         total: {totalPrice()}
       </p>
       <button onClick={() => clearCart(product => <ItemCart key={product.id} product={product}/>)}>Eliminar Todo</button>
+      <button onClick={handleClick}>Emitir Compra</button>
     </>
   )
 }
